0c5dd39136ff68077ca87734a613e2f500a4221a
cedapopovic <cedomirpopovic@eaton.com>
2024-07-02 12:58:32 +0200

    Update streamlit_app(1).py

--- streamlit_app(1).py
+++ streamlit_app(1).py
@@ -40,8 +40,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered', icon="✅")
 
 
-# New section to display fruitvice nutrition information
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
+
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
